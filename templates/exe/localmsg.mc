f8568b65927319e93a66ad8dc7b7a7d907e0fcb8
Nick Banks <nibanks@microsoft.com>
2023-12-18 09:26:57 -0500

    Add Localization to EXE Template (#10)

---
+++ templates/exe/localmsg.mc
@@ -0,0 +1,16 @@
+;//
+;// Local Messages for exe
+;// Message range: 10000+
+;//
+;
+MessageId=10000 SymbolicName=EXE_USAGE
+Language=English
+
+Usage: exe
+Options: NONE
+.
+
+MessageId=10001 SymbolicName=EXE_MESSAGE_1
+Language=English
+This is a sample message.
+.
--- templates/exe/main.c
+++ templates/exe/main.c
@@ -9,11 +9,32 @@ Abstract:
 --*/
 
 #include <windows.h>
+#include <stdio.h>
+#include "localmsg.h"
 
-void
-__cdecl
-wmain(_In_ int argc, _In_count_(argc) wchar_t **argv)
+void PutMsgW(_In_ UINT MsgNumber, ...)
+{
+    LPWSTR vp;
+    va_list arglist;
+    va_start(arglist, MsgNumber);
+    DWORD msglen =
+        FormatMessageW(
+            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_HMODULE,
+            NULL,
+            MsgNumber,
+            0L,
+            (LPWSTR)&vp,
+            0,
+            &arglist);
+    if (!msglen) {
+        wprintf(vp);
+        LocalFree(vp);
+    }
+}
+
+void __cdecl wmain(_In_ int argc, _In_count_(argc) wchar_t **argv)
 {
     UNREFERENCED_PARAMETER(argc);
     UNREFERENCED_PARAMETER(argv);
+    PutMsgW(EXE_USAGE);
 }
